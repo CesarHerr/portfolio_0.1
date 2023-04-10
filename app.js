4544181bc57b703cb1cffcfdc9e7cf0428d9ed0e
Juan Diego Guerra <jdxelric@gmail.com>
2023-04-10 17:51:45 -0500

    Update app.js
    Declare consts for hamburguer menu

---
+++ app.js
@@ -0,0 +1,6 @@
+const menuIcon = document.querySelector('.hamburger-btn');
+const menu = document.querySelector('.menu');
+const logo = document.querySelector('.logo-header');
+const closeBtn = document.querySelector('.hambuguer-close')
+const email = document.querySelector('.email-icon')
+const nav = document.querySelector('.nav-bar')
