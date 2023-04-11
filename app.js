73cc155295f343b92427d0a926d275168f6e8911
Cesar Herrera <cesarherr321@gmail.com>
2023-04-10 22:05:42 -0400

    Udpdate CSS and Js files
    Add fix styles nav-bar and fix js file using linters

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
 const menuIcon = document.querySelector('.hamburger-btn');
 const menu = document.querySelector('.menu');
 const logo = document.querySelector('.logo-header');
-const closeBtn = document.querySelector('.hambuguer-close')
-const email = document.querySelector('.email-icon')
-const nav = document.querySelector('.nav-bar')
+const closeBtn = document.querySelector('.hambuguer-close');
+const email = document.querySelector('.email-icon');
+const nav = document.querySelector('.nav-bar');
 
 menuIcon.addEventListener('click', () => {
   menu.classList.toggle('show');
   menuIcon.classList.toggle('show-icon');
-  logo.classList.toggle('visible')
-  closeBtn.classList.toggle('close')
-  email.classList.toggle('visible')
-  nav.classList.toggle('change-width')
-  
+  logo.classList.toggle('visible');
+  closeBtn.classList.toggle('close');
+  email.classList.toggle('visible');
+  nav.classList.toggle('change-width');
 });
 
 menu.addEventListener('click', () => {
   menu.classList.toggle('show');
   menuIcon.classList.toggle('show-icon');
-  logo.classList.toggle('visible')
-  closeBtn.classList.toggle('close')
-  email.classList.toggle('visible')
-  nav.classList.toggle('change-width')
-  
+  logo.classList.toggle('visible');
+  closeBtn.classList.toggle('close');
+  email.classList.toggle('visible');
+  nav.classList.toggle('change-width');
 });
