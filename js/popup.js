4caecbfc8d918c31109a81053fd67e6471270a87
Cesar Herrera <cesarherr321@gmail.com>
2023-04-12 01:15:29 -0400

    Update css file
    Add styles to the popup body on desktop version

--- js/popup.js
+++ js/popup.js
@@ -1,13 +1,13 @@
 const objectWorks = [
 {
     id: 'card_1',
-    title: 'Keeping track of hundreds  of components website',
+    title: ['Keeping track of hundreds  of components website','Multi Post Stories'],
     skills: ['HTML/CSS', 'Bootstrap', 'Ruby on Rails'],      
-    img: '../images/PopupDesktop.png',    
+    img: ['../images/PopupDesktop.png','../images/popicon.svg', '../images/gitpop.svg'],    
     buttonClose: '../images/ClosePopup.svg',
     btnSeeLive: 'index.html',
     btnSeeSource: 'index.html',
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. <br> <br> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita."
+    description: ["Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. <br> <br> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.","Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent"],
 },
 {
     id: 'card_1',
@@ -84,18 +84,22 @@ cardPopup.forEach((parameter, index) => {
     <div class="cardDesktopPopup">
         <img class="close" src="${objectWorks[index].buttonClose}" alt="">   
         <div class="popup-header">
-            <h2>${objectWorks[index].title}</h2>        
+            <h2>${objectWorks[index].title[0]}</h2>        
             <ul class="popup-skills">
                 <li>${objectWorks[index].skills[0]}</li>
                 <li>${objectWorks[index].skills[1]}</li>
                 <li>${objectWorks[index].skills[2]}</li>
             </ul>
         </div>
-        <img class="image-project" src="${objectWorks[index].img}" alt="imagen_project">
+        <img class="image-project" src="${objectWorks[index].img[0]}" alt="imagen_project">
         <div class="popup-description">
-            <p>${objectWorks[index].description}</p>        
-            <button class="btnSeeLive"><a href="${objectWorks[index].btnSeeLive}">See Live</a></button>
-            <button class="btnSeeSource"><a href="${objectWorks[index].btnSeeSource}">See Source</button>
+            <p>${objectWorks[index].description[0]}</p>        
+            <a href="${objectWorks[index].btnSeeLive}">
+                <button class="btnSeeLive">See Live<img src="${objectWorks[index].img[1]}" alt=""></button>
+            </a>    
+            <a href="${objectWorks[index].btnSeeSource}">
+                <button class="btnSeeSource">See Source<img src="${objectWorks[index].img[2]}" alt=""></button>
+            </a>     
         </div>
     </div>`;
 
