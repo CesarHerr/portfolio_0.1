9ea7416d6c7dc06bd03e95694f2d69bfdd9a7b1f
Cesar Herrera <cesarherr321@gmail.com>
2023-04-12 03:11:12 -0400

    Add styles mobile version  popup

--- js/popup.js
+++ js/popup.js
@@ -1,82 +1,83 @@
 const objectWorks = [
 {
-    id: 'card_1',
+    id: 'card_0',
     title: ['Keeping track of hundreds  of components website','Multi Post Stories'],
-    skills: ['HTML/CSS', 'Bootstrap', 'Ruby on Rails'],      
+    skills: ['HTML', 'Bootstrap', 'Ruby on Rails'],      
     img: ['../images/PopupDesktop.png','../images/popicon.svg', '../images/gitpop.svg'],    
     buttonClose: '../images/ClosePopup.svg',
     btnSeeLive: 'index.html',
     btnSeeSource: 'index.html',
-    description: ["Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. <br> <br> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.","Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent"],
+    description: ["Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. <br> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.","Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent"],
 },
 {
     id: 'card_1',
-    title: 'Keeping track of hundreds  of components website',
-    skills: ['HTML/CSS', 'Bootstrap', 'Ruby on Rails'],      
-    img: '../images/PopupDesktop.png',    
+    title: ['Keeping track of hundreds  of components website','Multi Post Stories'],
+    skills: ['HTML', 'Bootstrap', 'Ruby on Rails'],      
+    img: ['../images/PopupDesktop.png','../images/popicon.svg', '../images/gitpop.svg'],    
     buttonClose: '../images/ClosePopup.svg',
     btnSeeLive: 'index.html',
     btnSeeSource: 'index.html',
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. <br> <br> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita."
+    description: ["Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.  <br> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.","Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent"],
 },
 {
-    id: 'card_1',
-    title: 'Keeping track of hundreds  of components website',
-    skills: ['HTML/CSS', 'Bootstrap', 'Ruby on Rails'],      
-    img: '../images/PopupDesktop.png',    
+    id: 'card_2',
+    title: ['Keeping track of hundreds  of components website','Multi Post Stories'],
+    skills: ['HTML', 'Bootstrap', 'Ruby on Rails'],      
+    img: ['../images/PopupDesktop.png','../images/popicon.svg', '../images/gitpop.svg'],    
     buttonClose: '../images/ClosePopup.svg',
     btnSeeLive: 'index.html',
     btnSeeSource: 'index.html',
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. <br> <br> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita."
+    description: ["Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.  <br> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.","Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent"],
 },
 {
-    id: 'card_1',
-    title: 'Keeping track of hundreds  of components website',
-    skills: ['HTML/CSS', 'Bootstrap', 'Ruby on Rails'],      
-    img: '../images/PopupDesktop.png',    
+    id: 'card_3',
+    title: ['Keeping track of hundreds  of components website','Multi Post Stories'],
+    skills: ['HTML', 'Bootstrap', 'Ruby on Rails'],      
+    img: ['../images/PopupDesktop.png','../images/popicon.svg', '../images/gitpop.svg'],    
     buttonClose: '../images/ClosePopup.svg',
     btnSeeLive: 'index.html',
     btnSeeSource: 'index.html',
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. <br> <br> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita."
+    description: ["Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.  <br> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.","Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent"],
 },
 {
-    id: 'card_1',
-    title: 'Keeping track of hundreds  of components website',
-    skills: ['HTML/CSS', 'Bootstrap', 'Ruby on Rails'],      
-    img: '../images/PopupDesktop.png',    
+    id: 'card_4',
+    title: ['Keeping track of hundreds  of components website','Multi Post Stories'],
+    skills: ['HTML', 'Bootstrap', 'Ruby on Rails'],      
+    img: ['../images/PopupDesktop.png','../images/popicon.svg', '../images/gitpop.svg'],    
     buttonClose: '../images/ClosePopup.svg',
     btnSeeLive: 'index.html',
     btnSeeSource: 'index.html',
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. <br> <br> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita."
+    description: ["Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.  <br> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.","Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent"],
 },
 {
-    id: 'card_1',
-    title: 'Keeping track of hundreds  of components website',
-    skills: ['HTML/CSS', 'Bootstrap', 'Ruby on Rails'],      
-    img: '../images/PopupDesktop.png',    
+    id: 'card_5',
+    title: ['Keeping track of hundreds  of components website','Multi Post Stories'],
+    skills: ['HTML', 'Bootstrap', 'Ruby on Rails'],      
+    img: ['../images/PopupDesktop.png','../images/popicon.svg', '../images/gitpop.svg'],    
     buttonClose: '../images/ClosePopup.svg',
     btnSeeLive: 'index.html',
     btnSeeSource: 'index.html',
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. <br> <br> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita."
+    description: ["Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.  <br> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.","Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent"],
 },
 {
-    id: 'card_1',
-    title: 'Keeping track of hundreds  of components website',
-    skills: ['HTML/CSS', 'Bootstrap', 'Ruby on Rails'],      
-    img: '../images/PopupDesktop.png',    
+    id: 'card_6',
+    title: ['Keeping track of hundreds  of components website','Multi Post Stories'],
+    skills: ['HTML', 'Bootstrap', 'Ruby on Rails'],      
+    img: ['../images/PopupDesktop.png','../images/popicon.svg', '../images/gitpop.svg'],    
     buttonClose: '../images/ClosePopup.svg',
     btnSeeLive: 'index.html',
     btnSeeSource: 'index.html',
-    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. <br> <br> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita."
+    description: ["Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.  <br> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.","Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent"],
 }
+
 ];
 
-// this is for popup
+// this is for popup desktop
 const main = document.querySelector('body');
-const cardPopup = document.querySelectorAll('.see-btn');
+const cardPopup = document.querySelectorAll('.see-desktop');
 
 cardPopup.forEach((parameter, index) => {
-    parameter.addEventListener('click', () => {
+    parameter.addEventListener('click', () => {       
     const cards = document.createElement('div');
     cards.className = "popup-container"
 
@@ -94,6 +95,46 @@ cardPopup.forEach((parameter, index) => {
         <img class="image-project" src="${objectWorks[index].img[0]}" alt="imagen_project">
         <div class="popup-description">
             <p>${objectWorks[index].description[0]}</p>        
+            <a href="${objectWorks[index].btnSeeLive}">
+                <button class="btnSeeLive">See Live<img src="${objectWorks[index].img[1]}" alt=""></button>
+            </a>    
+            <a href="${objectWorks[index].btnSeeSource}">
+                <button class="btnSeeSource">See Source<img src="${objectWorks[index].img[2]}" alt=""></button>
+            </a>     
+        </div>
+    </div>`;
+
+    main.insertAdjacentElement('afterBegin', cards)
+    const close = document.querySelector('.close')
+    close.addEventListener('click',()=>{
+    main.removeChild(cards)
+    });
+    });
+});
+//popup movile version
+
+const mainMobile = document.querySelector('body');
+const cardPopupMobile = document.querySelectorAll('.see-mobile');
+
+cardPopupMobile.forEach((parameter, index) => {
+    parameter.addEventListener('click', () => {      
+    const cards = document.createElement('div');
+    cards.className = "popup-container"
+
+    cards.innerHTML =`
+    <div class="cardDesktopPopup">
+        <img class="close" src="${objectWorks[index].buttonClose}" alt="">   
+        <div class="popup-header">
+            <h2>${objectWorks[index].title[1]}</h2>        
+            <ul class="popup-skills">
+                <li>${objectWorks[index].skills[0]}</li>
+                <li>${objectWorks[index].skills[1]}</li>
+                <li>${objectWorks[index].skills[2]}</li>
+            </ul>
+        </div>
+        <img class="image-project" src="${objectWorks[index].img[0]}" alt="imagen_project">
+        <div class="popup-description">
+            <p>${objectWorks[index].description[1]}</p>        
             <a href="${objectWorks[index].btnSeeLive}">
                 <button class="btnSeeLive">See Live<img src="${objectWorks[index].img[1]}" alt=""></button>
             </a>    
