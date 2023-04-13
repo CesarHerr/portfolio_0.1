04e4b5195e48d3c4c5cbdbc4e627b6c29274e6ca
Juan Diego Guerra <jdxelric@gmail.com>
2023-04-13 14:32:43 -0500

    Update formV.js
    Declared constants for validation form

--- js/formV.js
+++ js/formV.js
@@ -1,1 +1,5 @@
-
+const formValidator = document.getElementById('form');
+const emailValidator = document.getElementById('email');
+const error= document.querySelector('.menssage-error')
+const errorMessage = document.createElement('div');
+errorMessage.classList.add('message');
