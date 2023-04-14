2bf3ad22932f25cdb7cfea94fde6cebe0ee190ec
Cesar Herrera <cesarherr321@gmail.com>
2023-04-13 21:00:09 -0400

    Update js file
    Add fix linter eslint

--- js/storage.js
+++ js/storage.js
@@ -1,24 +1,23 @@
-//storage
-
+// storage
+const formImput = document.querySelector('form');
 const nameInput = document.getElementById('name');
 const emailInput = document.getElementById('email');
 const messageInput = document.querySelector('textarea');
 
 const data = localStorage.getItem('formData');
-const storagedData = JSON.parse(data)
+const storagedData = JSON.parse(data);
 
 if (storagedData) {
- 
-    nameInput.value = storagedData.name;
-    emailInput.value = storagedData.email;
-    messageInput.value = storagedData.textArea;
-  }
+  nameInput.value = storagedData.name;
+  emailInput.value = storagedData.email;
+  messageInput.value = storagedData.textArea;
+}
 
-  form.addEventListener('input', () => {
-    const formData = {
-      name: nameInput.value,
-      email: emailInput.value,
-      textArea: messageInput.value,
-    };
-    localStorage.setItem('formData', JSON.stringify(formData));
-  });
+formImput.addEventListener('input', () => {
+  const formData = {
+    name: nameInput.value,
+    email: emailInput.value,
+    textArea: messageInput.value,
+  };
+  localStorage.setItem('formData', JSON.stringify(formData));
+});
