077a75985009e54cb78d8bb0361668c3feaaf879
Juan Diego Guerra <jdxelric@gmail.com>
2023-04-13 19:51:44 -0500

    Update storage.js
    Added conditional to verify if storageData has information in it.

--- js/storage.js
+++ js/storage.js
@@ -7,3 +7,9 @@ const messageInput = document.querySelector('textarea');
 const data = localStorage.getItem('formData');
 const storagedData = JSON.parse(data)
 
+if (storagedData) {
+ 
+    nameInput.value = storagedData.name;
+    emailInput.value = storagedData.email;
+    messageInput.value = storagedData.textArea;
+  }
