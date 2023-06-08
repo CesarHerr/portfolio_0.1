6e96be2ede4cb89e34d1cedea3d17ececda8cbb3
Cesar Herrera <cesarherr321@gmail.com>
2023-06-08 00:56:29 -0400

    Add styles buttons and transitions

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,7 @@
 const menuIcon = document.querySelector('.hamburger-btn');
 const menu = document.querySelector('.menu');
 const logo = document.querySelector('.logo-header');
-const closeBtn = document.querySelector('.hambuguer-close');
+const closeBtn = document.querySelector('.hamburger-close');
 const email = document.querySelector('.email-icon');
 const nav = document.querySelector('.nav-bar');
 
