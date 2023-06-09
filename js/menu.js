e3e579df66c8a8d9e2649d8744ebb904aa63a61e
Cesar Herrera <cesarherr321@gmail.com>
2023-06-08 21:53:40 -0400

    Add focus on nav-bar

--- js/menu.js
+++ js/menu.js
@@ -23,67 +23,58 @@ menu.addEventListener('click', () => {
   nav.classList.toggle('change-width');
 });
 
-const portfolio= document.querySelector('#portfolio')
-const portfolioLink= document.querySelector('.link-portfolio')
-
-portfolio.addEventListener("mouseover", mouseOver);
-portfolio.addEventListener("mouseout", mouseOut);
+const portfolio = document.querySelector('#portfolio');
+const portfolioLink = document.querySelector('.link-portfolio');
 
 function mouseOver() {
-  portfolioLink.style.fontSize = "1.5rem";
-  portfolioLink.style.padding = "0.7rem 2rem";
-  portfolioLink.style.boxShadow = "12.8px 20.6px 5.3px rgba(0, 0, 0, 0.194), 23.3px 37.6px 17.9px rgba(0, 0, 0, 0.286), 52px 84px 80px rgba(0, 0, 0, 0.48)";
-  portfolioLink.style.borderRadius = "0.8rem";
-  portfolioLink.style.border = "solid 1px rgba(0, 0, 0, 0.194)";  
-
+  portfolioLink.classList.toggle('linkedPortfolio');
 }
 
 function mouseOut() {
-  portfolioLink.style.padding = "0";
-  portfolioLink.style.boxShadow = "none";
-  portfolioLink.style.fontSize = "1rem";
-  portfolioLink.style.border = "none";
+  portfolioLink.classList.toggle('linkedPortfolio');
 }
 
-const about= document.querySelector('#about')
-const aboutLink= document.querySelector('.link-about')
+portfolio.addEventListener('mouseover', mouseOver);
+portfolio.addEventListener('mouseout', mouseOut);
 
-about.addEventListener("mouseover", mouseOver2);
-about.addEventListener("mouseout", mouseOut2);
+const about = document.querySelector('#about');
+const aboutLink = document.querySelector('.link-about');
 
 function mouseOver2() {
-  aboutLink.style.fontSize = "1.5rem";
-  aboutLink.style.padding = "0.7rem 2rem";
-  aboutLink.style.boxShadow = "12.8px 20.6px 5.3px rgba(0, 0, 0, 0.194), 23.3px 37.6px 17.9px rgba(0, 0, 0, 0.286), 52px 84px 80px rgba(0, 0, 0, 0.48)";
-  aboutLink.style.borderRadius = "0.8rem"
-  aboutLink.style.border = "solid 1px rgba(0, 0, 0, 0.194)";
-
+  aboutLink.classList.toggle('linkedAbout');
 }
 
 function mouseOut2() {
-  aboutLink.style.padding = "0";
-  aboutLink.style.boxShadow = "none";
-  aboutLink.style.fontSize = "1rem";
-  aboutLink.style.border = "none";
+  aboutLink.classList.toggle('linkedAbout');
 }
 
-const contact= document.querySelector('#contact')
-const contactLink= document.querySelector('.link-contact')
-const imgEmail= document.querySelector('.link-contact img')
+about.addEventListener('mouseover', mouseOver2);
+about.addEventListener('mouseout', mouseOut2);
 
-contact.addEventListener("mouseover", mouseOver3);
-contact.addEventListener("mouseout", mouseOut3);
+const contact = document.querySelector('#contact');
+const contactLink = document.querySelector('.link-contact');
 
 function mouseOver3() {
-contactLink.style.fontSize = "1.6rem";
-contactLink.style.padding = "0.7rem 2rem";
-contactLink.style.boxShadow = "12.8px 20.6px 5.3px rgba(0, 0, 0, 0.194), 23.3px 37.6px 17.9px rgba(0, 0, 0, 0.286), 52px 84px 80px rgba(0, 0, 0, 0.48)";
-contactLink.style.borderRadius = "0.8rem"
-contactLink.style.border = "solid 1px rgba(0, 0, 0, 0.194)";
-contactLink.style.border = "none";}
+  contactLink.classList.toggle('linkedContact');
+}
 
 function mouseOut3() {
-contactLink.style.padding = "0";
-contactLink.style.boxShadow = "none";
-contactLink.style.fontSize = "1rem";
+  contactLink.classList.toggle('linkedContact');
 }
+
+contact.addEventListener('mouseover', mouseOver3);
+contact.addEventListener('mouseout', mouseOut3);
+
+const headline = document.querySelector('.headline');
+const myLogo = document.querySelector('.logo-header');
+
+function mouseOver4() {
+  myLogo.classList.toggle('linkedLogo');
+}
+
+function mouseOut4() {
+  myLogo.classList.toggle('linkedLogo');
+}
+
+headline.addEventListener('mouseover', mouseOver4);
+headline.addEventListener('mouseout', mouseOut4);
